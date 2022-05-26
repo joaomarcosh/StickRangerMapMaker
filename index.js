c161dc429afc7c1798fbd3cb97799506129124f0
TimeGuardian <joaomarcosh@gmail.com>
2022-05-26 13:56:24 -0400

    Added grid and top layer

--- index.js
+++ index.js
@@ -1,87 +1,105 @@
-//const testW = 1043;
-//document.getElementById("esquerda").style.maxWidth=`${testW}px`;
-
 let tileCanvas = document.getElementById("tileCanvas");
 let mapCanvas = document.getElementById("mapCanvas");
+let topCanvas = document.getElementById("topCanvas");
+let gridCanvas = document.getElementById("gridCanvas");
 let tileCTX = tileCanvas.getContext("2d");
 let mapCTX = mapCanvas.getContext("2d");
+let topCTX = topCanvas.getContext("2d");
+let gridCTX = gridCanvas.getContext("2d");
 let tileCanvasContent;
 let mapCanvasContent;
+let topCanvasContent;
+let gridCanvasContent;
 let selectedTile;
 let mouseDown = false;
-let img1 = new Image();
+let currentCTX = mapCTX;
+let baseTileSet = new Image();
+baseTileSet.src = "./mt3.gif";
 
-img1.onload = function() {
-    tileCTX.drawImage(img1, 0, 0);
+baseTileSet.onload = function() {
+    tileCTX.drawImage(baseTileSet, 0, 0);
     tileCanvasContent = tileCTX.getImageData(0, 0, tileCanvas.width, tileCanvas.height);
     selectedTile = tileCTX.getImageData(0, 0, 16, 16);
-    drawRedBox(0,0);
-    //let img2 = new Image();
-    //img2.src = tileCanvas.toDataURL("image/jpeg");
+    drawRedBox(0,0,tileCTX);
 };
 
-img1.src = "./mt2.gif";
-
-function loadFile(event) {
-    let img2 = new Image();
-    img2.src = URL.createObjectURL(event.target.files[0]);
-    img2.onload = function() {
-        mapCanvas.width = mapCanvas.width;
-        mapCTX.drawImage(img2, 0, 0);
+function loadFile(event,canvas) {
+    let imageFile = new Image();
+    imageFile.src = URL.createObjectURL(event.target.files[0]);
+    imageFile.onload = function() {
+        canvas.width = canvas.width;
+        canvas.getContext("2d").drawImage(imageFile, 0, 0);
     }
-    console.log("cool!");
 }
 
 function changeCanvasSize() {
     newWidth = document.getElementById("mapCanvasW").value;
     newHeight = document.getElementById("mapCanvasH").value;
     mapCanvasContent = mapCTX.getImageData(0, 0, mapCanvas.width, mapCanvas.height);
+    topCanvasContent = topCTX.getImageData(0, 0, topCanvas.width, topCanvas.height);
     if (document.getElementById("tileOrPixel").checked) {
         mapCanvas.width = Math.ceil(newWidth)*16;
         mapCanvas.height = Math.ceil(newHeight)*16;
+        topCanvas.width = Math.ceil(newWidth)*16;
+        topCanvas.height = Math.ceil(newHeight)*16;
+        gridCanvas.width = Math.ceil(newWidth)*16;
+        gridCanvas.height = Math.ceil(newHeight)*16;
     } else {
         mapCanvas.width = Math.max(Math.ceil(newWidth/16)*16,16);
         mapCanvas.height = Math.max(Math.ceil(newHeight/16)*16,16);
+        topCanvas.width = Math.max(Math.ceil(newWidth/16)*16,16);
+        topCanvas.height = Math.max(Math.ceil(newHeight/16)*16,16);
+        gridCanvas.width = Math.max(Math.ceil(newWidth/16)*16,16);
+        gridCanvas.height = Math.max(Math.ceil(newHeight/16)*16,16);
     }
     mapCTX.putImageData(mapCanvasContent,0,0);
+    topCTX.putImageData(topCanvasContent,0,0);
+    drawGrid();
 }
 
 function pick(event) {
-    var x = Math.trunc(getMousePos(tileCanvas,event).x);
-    var y = Math.trunc(getMousePos(tileCanvas,event).y);
-    x=Math.floor(x/16)*16;
-    y=Math.floor(y/16)*16;
+    let {x,y} = getMousePos(tileCanvas,event);
     selectedTile = tileCTX.getImageData(x, y, 16, 16);
     tileCanvas.width = tileCanvas.width;
     tileCTX.putImageData(tileCanvasContent,0,0);
-    drawRedBox(x,y);
+    drawRedBox(x,y,tileCTX);
 }
 
 function place(event) {
-    var x = Math.trunc(getMousePos(mapCanvas,event).x);
-    var y = Math.trunc(getMousePos(mapCanvas,event).y);
-    x=Math.floor(x/16)*16;
-    y=Math.floor(y/16)*16;
-    mapCTX.putImageData(selectedTile,x,y)
+    let {x,y} = getMousePos(mapCanvas,event);
+    currentCTX.putImageData(selectedTile,x,y)
+}
+
+function setCurrentCanvas() {
+    if (document.getElementById("bottomRadio").checked) currentCTX=mapCTX
+    else if (document.getElementById("topRadio").checked) currentCTX=topCTX
+    else if (document.getElementById("bothRadio").checked) currentCTX=mapCTX
+    else console.log("something is fucked up!")
 }
 
 tileCanvas.addEventListener('click', function(event) {
     pick(event);
 });
 
-mapCanvas.addEventListener('mousedown', function(event) {
+topCanvas.addEventListener('mousedown', function(event) {
     mouseDown=true;
     place(event);
 });
-mapCanvas.addEventListener('mouseup', function() {
-    mouseDown=false;;
+topCanvas.addEventListener('mouseup', function() {
+    mouseDown=false;
 });
-mapCanvas.addEventListener('mousemove', function(event) {
+topCanvas.addEventListener('mousemove', function(event) {
     if (mouseDown) {
         place(event);
     }
+    let {x,y} = getMousePos(topCanvas,event);
+    gridCanvas.width = gridCanvas.width;
+    gridCTX.putImageData(gridCanvasContent,0,0);
+    drawRedBox(x,y,gridCTX);
 });
-
+//topCanvas.addEventListener('mousemove', function() {
+//    drawRedBox(0,0,gridCTX);
+//});
 
 function  getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect(), // abs. size of element
@@ -89,8 +107,8 @@ function  getMousePos(canvas, evt) {
       scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
   
     return {
-      x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-      y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+      x: Math.floor(Math.trunc((evt.clientX - rect.left) * scaleX)/16)*16,   // scale mouse coordinates after they have
+      y: Math.floor(Math.trunc((evt.clientY - rect.top) * scaleY)/16)*16     // been adjusted to be relative to element Math.floor(x/16)*16;
     }
 }
 
@@ -100,15 +118,29 @@ function download() {
     });
 }
 
-function drawRedBox(x,y,color="#FF0000") {
-    tileCTX.strokeStyle = color;
-    tileCTX.moveTo(x+0.5, y+0.5);
-    tileCTX.lineTo(x+0.5, y+15.5);
-    tileCTX.stroke();
-    tileCTX.lineTo(x+15.5, y+15.5);
-    tileCTX.stroke();
-    tileCTX.lineTo(x+15.5, y+0.5);
-    tileCTX.stroke();
-    tileCTX.lineTo(x+0.5, y+0.5);
-    tileCTX.stroke();
+function drawRedBox(x,y,context,color="#FF0000") {
+    context.beginPath();
+    context.strokeStyle = color;
+    context.moveTo(x+0.5, y+0.5);
+    context.lineTo(x+0.5, y+15.5);
+    context.lineTo(x+15.5, y+15.5);
+    context.lineTo(x+15.5, y+0.5);
+    context.lineTo(x+0.5, y+0.5);
+    context.stroke();
 }
+
+function drawGrid() {
+    gridCTX.strokeStyle = "#00000020";
+    for (x=16;x<gridCanvas.width;x+=16) {
+        gridCTX.moveTo(x-0.5,0.5);
+        gridCTX.lineTo(x-0.5,gridCanvas.height-0.5);
+    }
+    for (y=16;y<gridCanvas.height;y+=16) {
+        gridCTX.moveTo(0.5,y-0.5);
+        gridCTX.lineTo(gridCanvas.width-0.5,y-0.5);
+    }
+    gridCTX.stroke();
+    gridCanvasContent = gridCTX.getImageData(0, 0, gridCanvas.width, gridCanvas.height);
+}
+
+drawGrid();
