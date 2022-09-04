dde697fa08ff564e6b8d67b402b00a1251d5885e
joaomarcosh <joaomarcosh@gmail.com>
2022-09-04 15:08:03 -0400

    Fix loadFile()

--- page/helpers/loadFile.js
+++ page/helpers/loadFile.js
@@ -2,9 +2,9 @@ function loadFile(event,canvas) {
     let imageFile = new Image();
     imageFile.src = URL.createObjectURL(event.target.files[0]);
     imageFile.onload = function() {
-        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-        if (canvas===mapCanvas) paintCanvasBackground();
-        canvas.getContext("2d").drawImage(imageFile, 0, 0);
+        canvas.context.clearRect(0, 0, canvas.width, canvas.height);
+        if (canvas===mapCanvas) canvas.paintCanvasBackground();
+        canvas.context.drawImage(imageFile, 0, 0);
         if (canvas === tileCanvas) {
             tileCanvas.setContent();
             selectedTile = tileCanvas.context.getImageData(0, 0, 16, 16);
